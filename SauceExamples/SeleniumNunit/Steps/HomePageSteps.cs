3cf2203cec57ca6f999dc55b9e2c9288bc4d656c
nadvolod <nadvolod@gmail.com>
2018-07-12 11:51:22 -0400

    First working version of a SpecFlow test

---
+++ SauceExamples/SeleniumNunit/Steps/HomePageSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumNunit.Steps
+{
+    [Binding]
+    public class HomePageSteps
+    {
+        [When(@"the user opens Ultimate QA home page")]
+        public void WhenTheUserOpensUltimateQAHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the home page loads successfully")]
+        public void ThenTheHomePageLoadsSuccessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
