6055c13f64b09f8eff148bfc85a7ff1b7320299e
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2018-08-28 16:15:30 -0400

    added nunit selenium tests that run in parallel at the method level

--- SauceExamples/ParallelTestMethods/ParallelTestsAtMethodLevelWithSelenium.cs
+++ SauceExamples/ParallelTestMethods/ParallelTestsAtMethodLevelWithSelenium.cs
@@ -1,42 +1,66 @@
 ﻿using System;
 using System.Threading;
+using Common;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
 
 
 namespace ParallelTestMethods
 {
     [TestFixture]
-    [Category("Simple parallelized unit tests")]
+    [Category("Parallel Selenium tests")]
     public class ParallelTestsAtMethodLevelWithSelenium
     {
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new WebDriverFactory().CreateSauceDriver(TestContext.CurrentContext.Test.Name);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            var passed = TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed;
+            ((IJavaScriptExecutor)Driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
+            Driver?.Quit();
+        }
+        public IWebDriver Driver { get; set; }
+
         //This will default to the number of processors that a laptop has and will run that many threads at the same time
         [Test]
         [Parallelizable]
-        public void TestMethod1()
+        public void SeleniumTest1()
         {
+            PerformTestSteps();
+        }
+
+        private void PerformTestSteps()
+        {
+            Driver.Navigate().GoToUrl("https://www.google.com");
             Thread.Sleep(10000);
             Assert.Pass();
         }
+
         [Test]
         [Parallelizable]
-        public void TestMethod2()
+        public void SeleniumTest2()
         {
-            Thread.Sleep(10000);
-            Assert.Pass();
+            PerformTestSteps();
+
         }
         [Test]
         [Parallelizable]
-        public void TestMethod3()
+        public void SeleniumTest3()
         {
-            Thread.Sleep(10000);
-            Assert.Pass();
+            PerformTestSteps();
+
         }
         [Test]
         [Parallelizable]
-        public void TestMethod4()
+        public void SeleniumTest4()
         {
-            Thread.Sleep(10000);
-            Assert.Pass();
+            PerformTestSteps();
         }
     }
 }
