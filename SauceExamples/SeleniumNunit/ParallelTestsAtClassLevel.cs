7006160a366a86cfff004625ba2f4ef213cb44ac
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2018-08-29 17:51:01 -0400

    added working parallelized selenium tests at the class level. Update W3C example to work. But still get sauce labs error

--- SauceExamples/SeleniumNunit/ParallelTestsAtClassLevel.cs
+++ SauceExamples/SeleniumNunit/ParallelTestsAtClassLevel.cs
@@ -4,54 +4,46 @@ using OpenQA.Selenium;
 namespace SeleniumNunit
 {
     [TestFixture]
-    class ParallelizedTests
+    [Parallelizable]
+    [Category("Parallel selenium tests at the class level")]
+    class ParallelTestsAtClassLevel
     {
-        public IWebDriver Driver;
         [Test]
-        [Parallelizable]
-        public void Test()
+        public void Test1()
         {
             new ReusableTests().OpenBlogPage();
         }
+    }
+    [TestFixture]
+    [Parallelizable]
+    [Category("Parallel selenium tests at the class level")]
+
+    class ParallelizedTests2
+    {
         [Test]
-        [Parallelizable]
         public void Test2()
         {
             new ReusableTests().OpenBlogPage();
         }
+    }
+    [TestFixture]
+    [Parallelizable]
+    [Category("Parallel selenium tests at the class level")]
+    class ParallelizedTests3
+    {
         [Test]
-        [Parallelizable]
         public void Test3()
         {
             new ReusableTests().OpenBlogPage();
         }
     }
     [TestFixture]
-    class ParallelizedTests2
-    {
-        public IWebDriver Driver;
-        [Test]
-        public void Test()
-        {
-            new ReusableTests().OpenBlogPage();
-        }
-    }
-    [TestFixture]
-    class ParallelizedTests3
-    {
-        public IWebDriver Driver;
-        [Test]
-        public void Test()
-        {
-            new ReusableTests().OpenBlogPage();
-        }
-    }
-    [TestFixture]
+    [Parallelizable]
+    [Category("Parallel selenium tests at the class level")]
     class ParallelizedTests4
     {
-        public IWebDriver Driver;
         [Test]
-        public void Test()
+        public void Test4()
         {
             new ReusableTests().OpenBlogPage();
         }
