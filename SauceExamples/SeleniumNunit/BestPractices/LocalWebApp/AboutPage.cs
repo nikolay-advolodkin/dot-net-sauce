f9d3edcd5b322686685844df35eb8a4e70050b76
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2018-09-06 10:54:54 -0400

    moved the aboutpage to it's own class

---
+++ SauceExamples/SeleniumNunit/BestPractices/LocalWebApp/AboutPage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumNunit.BestPractices.LocalWebApp
+{
+    internal class AboutPage
+    {
+        private readonly IWebDriver _driver;
+
+        public AboutPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public bool IsLoaded
+        {
+            get
+            {
+                //using Thread.Sleep is a bad practice and here it is only done for demonstration purposes
+                Thread.Sleep(10000);
+                var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+                return wait.Until(ExpectedConditions.ElementIsVisible(By.TagName("h2"))).Displayed;
+            }
+        }
+
+        public AboutPage Open()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:44304/about");
+            return this;
+        }
+    }
+}
