bb8625e9d6c1793532fb4c8b154caa6ff5991757
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2018-09-17 12:27:25 -0400

    Moved sauce user name and password into SauceUser.cs

---
+++ SauceExamples/SeleniumNunit/BestPractices/CrossBrowserExamples/SauceLabsPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace SeleniumNunit.BestPractices.CrossBrowserExamples
+{
+    public class SauceLabsPage
+    {
+        private readonly IWebDriver _driver;
+
+        public SauceLabsPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public bool IsVisible
+        {
+            get
+            {
+                var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+                return wait.Until(ExpectedConditions.ElementIsVisible(By.Id("site-header"))).Displayed;
+            }
+        }
+
+        public SauceLabsPage Open()
+        {
+            _driver.Navigate().GoToUrl("https://www.saucelabs.com");
+            return this;
+        }
+    }
+}
