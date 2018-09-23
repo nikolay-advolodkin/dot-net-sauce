4f50e9cd204a09775125549cfba55cc076d06951
nadvolod <nadvolod@gmail.com>
2018-09-23 18:49:41 -0400

    add selenium on MSTest framework

---
+++ SauceExamples/SeleniumMsTestDotNetFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumMsTestDotNetFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumMsTestDotNetFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3eeee112-9726-481b-b183-cfc7214be6bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SauceExamples/SeleniumMsTestDotNetFramework/SimpleSauceTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace SeleniumMsTestDotNetFramework
+{
+    [TestClass]
+    public class SimpleSauceTest
+    {
+        IWebDriver Driver;
+        [TestMethod]
+        public void SauceConnectTest()
+        {
+            //TODO please supply your Sauce Labs user name in an environment variable
+            var sauceUserName = Environment.GetEnvironmentVariable("SAUCE_USERNAME", EnvironmentVariableTarget.User);
+            //TODO please supply your own Sauce Labs access Key in an environment variable
+            var sauceAccessKey = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY", EnvironmentVariableTarget.User);
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddAdditionalCapability(CapabilityType.Version, "latest", true);
+            options.AddAdditionalCapability(CapabilityType.Platform, "Windows 10", true);
+            options.AddAdditionalCapability("username", sauceUserName, true);
+            options.AddAdditionalCapability("accessKey", sauceAccessKey, true);
+            options.AddAdditionalCapability("name", "SauceConnectTest", true);
+
+            Driver =  new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), options.ToCapabilities(),
+                TimeSpan.FromSeconds(600));
+            Driver.Navigate().GoToUrl("https://www.google.com");
+            Assert.IsTrue(true);
+        }
+
+        [TestCleanup]
+        public void CleanUpAfterEveryTestMethod()
+        {
+            //var passed = TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed;
+            ((IJavaScriptExecutor)Driver).ExecuteScript("sauce:job-result=" + "passed");
+            Driver?.Quit();
+        }
+    }
+}
