55e896ab39a98fc647a2e4a0d84982eddfdced6b
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2018-09-24 15:46:26 -0400

    added unit and integration tests

---
+++ SauceExamples/Integration.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Integration.Tests
+{
+    [TestClass]
+    [TestCategory("integration")]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldPass()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
