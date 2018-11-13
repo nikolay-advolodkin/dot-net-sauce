fc1ad91f156579cc22e850cb4d436668562b44e0
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2018-11-13 11:36:34 -0500

    Created a new project to run on RDC + MSTest + and latest Appium version using Desired Caps Added another project that uses a beta version of Appium with AppiumCapabilities instead of DesiredCaps

---
+++ SauceExamples/AppiumLatestOnDotNetFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AppiumLatestOnDotNetFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppiumLatestOnDotNetFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99f99e3a-b9a8-46d8-ac32-fc40552d04cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
