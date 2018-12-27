a76b44a4d571b9a049c7e39508aead213cf50f83
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2018-12-27 10:14:43 -0500

    Add powershell to test sauce connect state

---
+++ Test-SauceConnectState.Tests.ps1
@@ -0,0 +1,36 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-KgpConnectionStatus" {
+    $fakeResponse = New-Object -TypeName psobject 
+    $fakeResponse | Add-Member -MemberType NoteProperty -Name StatusCode -Value 200
+
+    Mock ConvertFrom-Json {}
+    It "Should return true if tunnel is up" {
+        $result = Get-KgpConnectionStatus $fakeResponse
+        Assert-MockCalled ConvertFrom-Json -Times 1
+        #Get-KgpConnectionStatus $fakeResponse.StatusCode | Should -Be $true
+    }
+    $fakeResponse.StatusCode = 404     
+    It "Should return false if tunnel is down" {
+        Get-KgpConnectionStatus $fakeResponse | Should -Be $false
+    }
+}
+Describe "Test-SauceConnectStatus" {
+    Mock Get-KgpConnectionStatus {return $false}
+    Mock Start-Sleep {}
+    Mock Send-Notification {}
+    $result = Test-SauceConnectStatus -IsInfiniteLoop $false
+    It "Should send notification" {
+        Assert-MockCalled Send-Notification -Times 1
+    }
+
+    Mock Get-KgpConnectionStatus {return $true}
+    Mock Start-Sleep {}
+    Mock Send-Notification {}
+    $result = Test-SauceConnectStatus -IsInfiniteLoop $false
+    It "Should send notification" {
+        Assert-MockCalled Start-Sleep -Times 1
+    }
+}
---
+++ Test-SauceConnectState.ps1
@@ -0,0 +1,33 @@
+﻿$IntervalForRestart = 120
+
+function Get-KgpConnectionStatus($Response) {
+    if ($Response.StatusCode -eq 200) {
+        $json = ConvertFrom-Json $Response.Content
+        return $json.kgp.Connected
+    }
+    return $false
+}
+function Invoke-SauceConnectDebugUrl() {
+    try { Invoke-WebRequest -Uri http://localhost:8888/debug/vars -Method get }
+    catch { return $false }    
+}
+function Test-SauceConnectStatus($IsInfiniteLoop = $true) {
+    do {
+        $IsConnected = Get-KgpConnectionStatus -Response (Invoke-SauceConnectDebugUrl)
+        if($IsConnected -ne $true)
+        {
+            Send-Notification
+            Wait-SauceConnectToRestart -SecondsToWait $IntervalForRestart
+        }
+        else
+        {
+            Start-Sleep -Seconds 15
+        }
+    } while ($IsInfiniteLoop)
+}
+function Send-Notification() {
+    return Write-Host "Sauce Connect is down! Please restart."
+}
+function Wait-SauceConnectToRestart($SecondsToWait) {
+    return Start-Sleep -Seconds $SecondsToWait
+}
