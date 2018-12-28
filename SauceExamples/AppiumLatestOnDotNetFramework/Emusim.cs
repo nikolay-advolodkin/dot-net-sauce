ecb2252be428c7812fad7ffff165dd0c1438d42a
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2018-12-28 11:03:02 -0500

    add a test for emusim with android. Downgrade Selenium version so that it can work with the latest Appium version

---
+++ SauceExamples/AppiumLatestOnDotNetFramework/Emusim.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+
+namespace AppiumLatestOnDotNetFramework
+{
+    [TestClass]
+    [TestCategory("MsTest")]
+    [TestCategory("Emusim")]
+    [TestCategory("Android")]
+
+    public class Emusim
+    {
+        [TestMethod]
+        public void Android71()
+        {
+            var capabilities = new DesiredCapabilities();
+            //set your user name and access key to run tests in Sauce
+            capabilities.SetCapability("username", SauceUser.Name);
+            //set your sauce labs access key
+            capabilities.SetCapability("accessKey", SauceUser.AccessKey);
+            //capabilities.SetCapability("appiumVersion", "1.9.1");
+            capabilities.SetCapability("deviceName", "Samsung Galaxy Tab A 10 GoogleAPI Emulator");
+            capabilities.SetCapability("deviceOrientation", "portrait");
+            capabilities.SetCapability("browserName", "Chrome");
+            capabilities.SetCapability("platformVersion", "7.1");
+            capabilities.SetCapability("platformName", "Android");
+            capabilities.SetCapability("name", MethodBase.GetCurrentMethod().Name);
+            capabilities.SetCapability("newCommandTimeout", 90);
+
+            var driver = new AndroidDriver<IWebElement>(new SauceLabsData().SauceHubUri, capabilities);
+            //var logEntries = driver.Manage().Logs.GetLog("driver");
+            driver.Navigate().GoToUrl("https://www.saucedemo.com");
+            Assert.IsTrue(true);
+            driver.Quit();
+        }
+    }
+}
--- SauceExamples/Common/SauceLabsData.cs
+++ SauceExamples/Common/SauceLabsData.cs
@@ -1,7 +1,10 @@
-﻿namespace Common
+﻿using System;
+
+namespace Common
 {
     public class SauceLabsData
     {
         public string SauceHubUrl => "https://ondemand.saucelabs.com/wd/hub";
+        public Uri SauceHubUri => new Uri(SauceHubUrl);
     }
 }
