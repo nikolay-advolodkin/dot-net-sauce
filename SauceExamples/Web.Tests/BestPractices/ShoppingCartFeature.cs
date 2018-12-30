38d8b9cceefc9e21192d8298aa91ca632580da69
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2018-12-30 15:35:05 -0500

    finished adding all of the tests for the best practices parallelization

--- SauceExamples/Web.Tests/BestPractices/ShoppingCartFeature.cs
+++ SauceExamples/Web.Tests/BestPractices/ShoppingCartFeature.cs
@@ -4,44 +4,52 @@ using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
+using SeleniumNunit.BestPractices.CrossBrowserExamples;
 using Web.Tests.Pages;
 
 namespace Web.Tests.BestPractices.Tests
 {
     [TestFixture]
     [Parallelizable]
-    public class ShoppingCartFeature
+    [TestFixtureSource(typeof(CrossBrowserData), "LatestConfigurations")]
+    public class ShoppingCartFeature : BaseCrossBrowserTest
     {
-        private IWebDriver _driver;
+        public ShoppingCartFeature(string browser, string browserVersion, string osPlatform) : 
+            base(browser, browserVersion, osPlatform)
+        {
+        }
+
         [Test]
         public void ShouldBeAbleToCheckOutWithItems()
         {
-            _driver = new WebDriverFactory().CreateSauceDriver(MethodBase.GetCurrentMethod().Name);
-            var checkoutPage = new CheckoutPage(_driver);
-            checkoutPage.GoTo();
-            checkoutPage.Cart.SetCartState()
-                .HasItems.Should().BeTrue("The cart should have some items in it since they were injected.");
-            var checkoutCompletePage = checkoutPage.Finish();
-            checkoutCompletePage.IsCheckedOut.Should().BeTrue("The checkout process should redirect us to the success page.");
+            //Arrange
+            var shoppingCartPage = new YourShoppingCartPage(Driver).Open();
+            //We don't need to actually use th UI to add items to the cart. 
+            //I'm injecting Javascript to control the state of the cart
+            shoppingCartPage.Cart.SetCartState();
+            //Act
+            var checkoutOverviewPage = shoppingCartPage.Checkout().
+                FillOutPersonalInformation();    
+            //Assert
+            checkoutOverviewPage.FinishCheckout().
+                IsCheckoutComplete.Should().BeTrue("we finished the checkout process");
+        }
+        [Test]
+        public void ShouldBeAbleToAddItemToCart()
+        {
+            //Arrange
+            var productsPage = new ProductsPage(Driver);
+            //Act
+            productsPage.Open();
+            productsPage.AddToCart(Item.Backpack);
+            //Assert
+            productsPage.Cart.ItemCount.Should().Be(1, "we added a backpack to the cart");
         }
 
-        ////validate that a product can be added to a cart
-        //productsPage.AddToCart(Item.Backpack);
-        //    productsPage.Cart.ItemCount.Should().Be(1, "we added a backpack to the cart");
-
-        ////validate that user can checkout
-        //var cartPage = productsPage.Cart.Click();
-        //var checkoutOverviewPage = cartPage.Checkout().
-        //    FillOutPersonalInformation();
-        //checkoutOverviewPage.FinishCheckout().IsCheckoutComplete.
-        //    Should().
-        //        BeTrue("we finished the checkout process");
-        [TearDown]
-        public void CleanUpAfterEveryTestMethod()
+        [SetUp]
+        public void RunBeforeEveryTest()
         {
-            var passed = TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed;
-            ((IJavaScriptExecutor)_driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
-            _driver?.Quit();
+            SauceReporter.SetBuildName("BestPracticesTests");
         }
     }
 }
--- SauceExamples/Web.Tests/Pages/YourShoppingCartPage.cs
+++ SauceExamples/Web.Tests/Pages/YourShoppingCartPage.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium;
+﻿using System;
+using OpenQA.Selenium;
+using Web.Tests.Elements;
 
 namespace Web.Tests.Pages
 {
@@ -8,10 +10,18 @@ namespace Web.Tests.Pages
         {
         }
 
+        public CartElement Cart => new CartElement(_driver);
+
         internal CheckoutInformationPage Checkout()
         {
             Wait.UntilIsVisibleByClass("cart_checkout_link").Click();
             return new CheckoutInformationPage(_driver);
+        }
+
+        internal YourShoppingCartPage Open()
+        {
+            _driver.Navigate().GoToUrl($"{BaseUrl}/cart.html");
+            return this;
         }
     }
 }
