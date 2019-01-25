0bd062b89255dbb7bd8c23c209ec7c8253f91f16
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2019-01-25 11:30:58 -0500

    renamed another APpium project so that it's name makes sense

--- SauceExamples/AppiumLatestOnDotNetFramework/Emusim.cs
+++ SauceExamples/AppiumLatestOnDotNetFramework/Emusim.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium.Android;
 
-namespace AppiumLatestOnDotNetFramework
+namespace AppiumMsTest
 {
     [TestClass]
     [TestCategory("MsTest")]
