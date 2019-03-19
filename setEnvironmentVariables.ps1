776df6b02c9ff81582ec39ee67ecdbd1cedf2f37
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2019-03-19 17:48:16 -0400

    Fix write-host to write-output

--- setEnvironmentVariables.ps1
+++ setEnvironmentVariables.ps1
@@ -3,8 +3,8 @@ Param(
 [string]$sauceUserName,
 [string]$sauceAccessKey
 )
-Write-Host "sauce.userName that was passed in from Azure DevOps=>$sauceUserName"
-Write-Host "sauce.accessKey that was passed in from Azure DevOps=>$sauceAccessKey"
+Write-Output "sauce.userName that was passed in from Azure DevOps=>$sauceUserName"
+Write-Output "sauce.accessKey that was passed in from Azure DevOps=>$sauceAccessKey"
 
 [Environment]::SetEnvironmentVariable("SAUCE_USERNAME", "$sauceUserName", "User")
 [Environment]::SetEnvironmentVariable("SAUCE_ACCESS_KEY", "$sauceAccessKey", "User")
