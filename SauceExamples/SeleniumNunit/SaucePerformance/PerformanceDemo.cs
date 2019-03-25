72efbf0aff43eef64e083d14a2195a52c5e7a069
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2019-03-25 13:37:25 -0400

    1st working test. Performance doesn't work yet

---
+++ SauceExamples/SeleniumNunit/SaucePerformance/PerformanceDemo.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+
+namespace SeleniumNunit.SaucePerformance
+{
+    [TestFixture]
+    public class PerformanceDemo
+    {
+        [Test]
+        public void PerformanceTest()
+        {
+            var sauceUserName = Environment.GetEnvironmentVariable("SAUCE_USERNAME", EnvironmentVariableTarget.User);
+            var sauceAccessKey = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY", EnvironmentVariableTarget.User);
+
+            var chromeOptions = new ChromeOptions()
+            {
+                BrowserVersion = "latest",
+                PlatformName = "Windows 10",
+                UseSpecCompliantProtocol = true
+            };
+            var sauceOptions = new Dictionary<string, object>
+            {
+                ["username"] = sauceUserName,
+                ["accessKey"] = sauceAccessKey,
+                ["name"] = TestContext.CurrentContext.Test.Name,
+                ["extendedDebugging"] = true,
+                ["capturePerformance"] = true
+            };
+            chromeOptions.AddAdditionalCapability("sauce:options", sauceOptions, true);
+
+            var driver = new RemoteWebDriver(new Uri("https://ondemand.saucelabs.com/wd/hub"),
+                chromeOptions.ToCapabilities(), TimeSpan.FromSeconds(600));
+            driver.Navigate().GoToUrl("https://www.saucedemo.com");
+            driver.Quit();
+        }
+    }
+}
