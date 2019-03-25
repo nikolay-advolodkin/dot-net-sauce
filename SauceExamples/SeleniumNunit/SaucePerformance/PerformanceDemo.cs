4351e3e60cebd86e13b765bcb476bd0a06682432
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2019-03-25 14:46:29 -0400

    Working performance test with W3C

--- SauceExamples/SeleniumNunit/SaucePerformance/PerformanceDemo.cs
+++ SauceExamples/SeleniumNunit/SaucePerformance/PerformanceDemo.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using Common;
+using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
 using System;
@@ -10,6 +12,7 @@ namespace SeleniumNunit.SaucePerformance
     public class PerformanceDemo
     {
         [Test]
+        [Repeat(5)]
         public void PerformanceTest()
         {
             var sauceUserName = Environment.GetEnvironmentVariable("SAUCE_USERNAME", EnvironmentVariableTarget.User);
@@ -34,6 +37,14 @@ namespace SeleniumNunit.SaucePerformance
             var driver = new RemoteWebDriver(new Uri("https://ondemand.saucelabs.com/wd/hub"),
                 chromeOptions.ToCapabilities(), TimeSpan.FromSeconds(600));
             driver.Navigate().GoToUrl("https://www.saucedemo.com");
+
+            var isPassed = TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Passed;
+            new SauceJavaScriptExecutor(driver).LogTestStatus(isPassed);
+
+            //var logType = new Dictionary<string, object>();
+            //logType.Add("type", "sauce:performance");
+
+            //var performanceMetrics = ((IJavaScriptExecutor) driver).ExecuteScript("sauce:performance", logType);
             driver.Quit();
         }
     }
